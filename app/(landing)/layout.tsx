3a3b8a61c97c7626a110c678998675a8ef136789
WebDevMullins <webdevmullins@gmail.com>
2024-02-26 18:23:18 -0600

    add navbar and footer to landing page

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,11 +1,14 @@
-const LandingLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-full bg-slate-100">
-      {/* TODO: Add header */}
-      <main className="bg-slate-100 pb-20 pt-40">{children}</main>
-      {/* TODO: Add footer */}
-    </div>
-  );
-};
+import { Footer } from './_components/footer'
+import { Navbar } from './_components/navbar'
 
-export default LandingLayout;
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className='h-full bg-slate-100'>
+			<Navbar />
+			<main className='bg-slate-100 pb-20 pt-40'>{children}</main>
+			<Footer />
+		</div>
+	)
+}
+
+export default LandingLayout
