62429742845d967c124d62e9cca70a051be43f84
WebDevMullins <webdevmullins@gmail.com>
2024-02-27 16:31:35 -0600

    working on sidebar

---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+export type Organization = {
+	id: string
+	slug: string
+	imageUrl: string
+	name: string
+}
+
+interface NavItemProps {
+	isActive: boolean
+	isExpanded: boolean
+	organization: Organization
+	onExpand: (id: string) => void
+}
+
+const NavItem = ({
+	isActive,
+	isExpanded,
+	organization,
+	onExpand
+}: NavItemProps) => {
+	return <div>NavItem</div>
+}
+export default NavItem
