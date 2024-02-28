3c9178e5f5d4e03dd186e1ccd96f7852c93c3896
WebDevMullins <webdevmullins@gmail.com>
2024-02-28 16:16:13 -0600

    Refactor board-list component and add suspense fallback

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,9 +1,29 @@
+import { auth } from '@clerk/nextjs'
 import { HelpCircleIcon, User2Icon } from 'lucide-react'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 import FormPopover from '@/components/form/form-popover'
 import Hint from '@/components/hint'
+import { Skeleton } from '@/components/ui/skeleton'
+import { db } from '@/lib/db'
 
-const BoardList = () => {
+const BoardList = async () => {
+	const { orgId } = auth()
+
+	if (!orgId) {
+		return redirect('/select-org')
+	}
+
+	const boards = await db.board.findMany({
+		where: {
+			orgId
+		},
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
+
 	return (
 		<div className='space-y-4'>
 			<div className='flex items-center text-lg font-semibold text-neutral-700'>
@@ -11,9 +31,19 @@ const BoardList = () => {
 					size={24}
 					className='mr-2'
 				/>
-				Your boards
+				Your Boards
 			</div>
 			<div className='grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4'>
+				{boards.map((board) => (
+					<Link
+						key={board.id}
+						href={`/board/${board.id}`}
+						className='group relative aspect-video h-full w-full overflow-hidden rounded-sm bg-sky-700 bg-cover bg-center bg-no-repeat p-2'
+						style={{ backgroundImage: `url(${board.imageThumbUrl})` }}>
+						<div className='absolute inset-0 bg-black/30 transition group-hover:bg-black/40' />
+						<p className='relative font-semibold text-white'>{board.title}</p>
+					</Link>
+				))}
 				<FormPopover
 					side='right'
 					sideOffset={10}>
@@ -36,3 +66,15 @@ const BoardList = () => {
 	)
 }
 export default BoardList
+
+BoardList.Skeleton = function SkeletonBoardList() {
+	return (
+		<div className='grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4'>
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+			<Skeleton className='aspect-video h-full w-full p-2' />
+		</div>
+	)
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import { Separator } from '@/components/ui/separator'
 import BoardList from './_components/board-list'
 import Info from './_components/info'
@@ -8,7 +10,9 @@ const OrganizationIdPage = () => {
 			<Info />
 			<Separator className='my-4' />
 			<div className='px-2 md:px-4'>
+				<Suspense fallback={<BoardList.Skeleton />}>
 				<BoardList />
+				</Suspense>
 			</div>
 		</div>
 	)
