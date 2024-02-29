9dae359843e693be90e548f6b4f1d2b113be8f60
WebDevMullins <webdevmullins@gmail.com>
2024-02-29 11:38:07 -0600

    add board navbar and delete board functionality

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { MoreHorizontalIcon, Trash2Icon, XIcon } from 'lucide-react'
+import { toast } from 'sonner'
+
+import { deleteBoard } from '@/actions/delete-board'
+import { useAction } from '@/hooks/use-action'
+
+import { Button } from '@/components/ui/button'
+import {
+	Popover,
+	PopoverClose,
+	PopoverContent,
+	PopoverTrigger
+} from '@/components/ui/popover'
+
+interface BoardOptionsProps {
+	id: string
+}
+
+export const BoardOptions = ({ id }: BoardOptionsProps) => {
+	const { execute, isLoading } = useAction(deleteBoard, {
+		onComplete() {
+			toast.success('Board deleted')
+		},
+		onError(error) {
+			toast.error(error)
+		}
+	})
+
+	const onDelete = () => {
+		execute({ id })
+	}
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button
+					variant='transparent'
+					className='h-auto w-auto p-2'>
+					<MoreHorizontalIcon size={16} />
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent
+				className='px-0 py-3'
+				side='bottom'
+				align='start'>
+				<div className='pb-4 text-center text-sm font-medium text-neutral-600'>
+					Board Actions
+				</div>
+				<PopoverClose asChild>
+					<Button
+						variant='ghost'
+						className='absolute right-2 top-2 h-auto w-auto p-2 text-neutral-600'>
+						<XIcon size={16} />
+					</Button>
+				</PopoverClose>
+				<Button
+					variant='ghost'
+					onClick={onDelete}
+					disabled={isLoading}
+					className='h-auto w-full justify-start rounded-none p-2 px-5 text-sm font-normal text-rose-500 hover:bg-rose-500/90'>
+					<Trash2Icon
+						size={16}
+						className='mr-2'
+					/>
+					Delete Board
+				</Button>
+			</PopoverContent>
+		</Popover>
+	)
+}
---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,68 @@
+import { auth } from '@clerk/nextjs'
+import { startCase } from 'lodash'
+import { notFound, redirect } from 'next/navigation'
+
+import { db } from '@/lib/db'
+
+import { BoardNavbar } from './_components/board-navbar'
+
+export async function generateMetadata({
+	params
+}: {
+	params: { boardId: string }
+}) {
+	const { orgId } = auth()
+
+	if (!orgId) {
+		return {
+			title: 'Board'
+		}
+	}
+
+	const board = await db.board.findFirst({
+		where: {
+			id: params.boardId,
+			orgId
+		}
+	})
+
+	return {
+		title: startCase(board?.title) || 'Board'
+	}
+}
+
+const BoardIdLayout = async ({
+	children,
+	params
+}: {
+	children: React.ReactNode
+	params: { boardId: string }
+}) => {
+	const { orgId } = auth()
+
+	if (!orgId) {
+		redirect('/select-org')
+	}
+
+	const board = await db.board.findFirst({
+		where: {
+			id: params.boardId,
+			orgId
+		}
+	})
+
+	if (!board) {
+		notFound()
+	}
+
+	return (
+		<div
+			className='relative h-full bg-cover bg-center bg-no-repeat'
+			style={{ backgroundImage: `url(${board.imageFullUrl})` }}>
+			<BoardNavbar data={board} />
+			<div className='absolute inset-0 bg-black/10' />
+			<main className='relative h-full pt-28'>{children}</main>
+		</div>
+	)
+}
+export default BoardIdLayout
