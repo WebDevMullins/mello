87b318b19a57950bb659ff1da791a0577c6250bb
WebDevMullins <webdevmullins@gmail.com>
2024-02-29 13:49:47 -0600

    add list item and ability to edit title

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,8 +2,10 @@
 
 import { ListWithCards } from '@/types'
 import { List } from '@prisma/client'
+import { useEffect, useState } from 'react'
 
 import { ListForm } from './list-form'
+import { ListItem } from './list-item'
 
 interface ListContainerProps {
 	boardId: string
@@ -11,8 +13,22 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+	const [orderedData, setOrderedData] = useState(data)
+
+	useEffect(() => {
+		setOrderedData(data)
+	}, [data])
 	return (
-		<ol>
+		<ol className='flex h-full gap-x-3'>
+			{orderedData.map((list, index) => {
+				return (
+					<ListItem
+						key={list.id}
+						index={index}
+						data={list}
+					/>
+				)
+			})}
 			<ListForm />
 			<div className='w-1 flex-shrink-0' />
 		</ol>
