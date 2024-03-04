9440c5afe01a5d124b394175cfc65a19b948bdb9
WebDevMullins <webdevmullins@gmail.com>
2024-03-04 15:15:36 -0600

    Refactor landing page layout

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 
 const LandingPage = () => {
 	return (
-		<main className='flex flex-col items-center'>
+		<section className='flex flex-col items-center px-4'>
 			<div className='flex flex-col items-center justify-center'>
 				<h1 className='mb-6 text-center text-3xl md:text-6xl'>
 					Mello helps teams move
@@ -14,7 +14,7 @@ const LandingPage = () => {
 					work forward.
 				</div>
 			</div>
-			<div className='mx-auto mt-4 max-w-sm text-center text-sm text-primary/60 md:max-w-2xl md:text-xl'>
+			<div className='mx-auto mt-4 max-w-xs text-center text-sm text-primary/60 md:max-w-2xl md:text-xl'>
 				Collaborate, manage projects, and reach new productivity peaks. From
 				high rises to the home office, the way your team works is unique -
 				accomplish it all with Mello.
@@ -25,7 +25,7 @@ const LandingPage = () => {
 				variant='default'>
 				<Link href='/sign-up'>Get Mello for free</Link>
 			</Button>
-		</main>
+		</section>
 	)
 }
 
